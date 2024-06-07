cdc0dd7af96cb28d8870162dffcb8ba2babeeceb
Boran Ozkan <boranozkan11@gmail.com>
2024-06-07 18:04:28 +0300

    Created Gravity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private CharacterController characterController;
+    private float gravity = -9.81f;
+    private Vector3 gravityVector;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +17,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 movement = (Input.GetAxis("Horizontal") * transform.right) + (Input.GetAxis("Vertical") * transform.forward);
+        gravityVector.y += gravity * Time.deltaTime * Time.deltaTime;
+        Vector3 movement = (Input.GetAxis("Horizontal") * transform.right) + (Input.GetAxis("Vertical") * transform.forward) + (gravityVector * 10);
         characterController.Move(movement * 10 * Time.deltaTime);
     }
 }
