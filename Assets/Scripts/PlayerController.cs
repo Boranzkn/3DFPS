1dfa1d306c4b97c2a0f1cb006987bd5dfd643625
Boran Ozkan <boranozkan11@gmail.com>
2024-06-07 18:24:53 +0300

    Created Ground Checker

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private CharacterController characterController;
+
+    // Gravity
     private float gravity = -9.81f;
     private Vector3 gravityVector;
+
+    // Ground Checker
+    [SerializeField] private Transform groundCheckerTransform;
+    [SerializeField] private LayerMask groundLayer;
+    private float groundCheckerRadius = 0.35f;
+    private bool isGrounded = false;
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +28,12 @@ public class PlayerController : MonoBehaviour
         gravityVector.y += gravity * Time.deltaTime * Time.deltaTime;
         Vector3 movement = (Input.GetAxis("Horizontal") * transform.right) + (Input.GetAxis("Vertical") * transform.forward) + (gravityVector * 10);
         characterController.Move(movement * 10 * Time.deltaTime);
+
+        // Ground Checker
+        isGrounded = Physics.CheckSphere(groundCheckerTransform.position, groundCheckerRadius, groundLayer);
+        if (isGrounded)
+        {
+            gravityVector.y = -3;
+        }
     }
 }
