c0955c0421ceb1482a162e85fa9f5f08823aefc6
Boran Ozkan <boranozkan11@gmail.com>
2024-06-24 15:19:27 +0300

    Added Enemy AI

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -2,21 +2,106 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyManager : MonoBehaviour
 {
     private int health = 100;
 
+    //Navmesh
+    private NavMeshAgent agent;
+    private Transform playerTransform;
+    public LayerMask groundLayer;
+    public LayerMask playerLayer;
+
+    //Patrolling
+    private Vector3 walkPoint;
+    private float walkPointRange = 5f;
+    private bool walkPointSet;
+
+    //Detecting
+    private float sightRange = 10f, attackRange = 5f;
+    private bool isInSightRange, isInAttackRange;
+
+    //Attacking
+    private float attackDelay = 1f;
+    private bool isAttacking;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        playerTransform = GameObject.Find("Player").transform;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        isInSightRange = Physics.CheckSphere(transform.position, sightRange, playerLayer);
+        isInAttackRange = Physics.CheckSphere(transform.position, attackRange, playerLayer);
+
+        if (!isInSightRange && !isInAttackRange)
+        {
+            Patrolling();
+        }
+        else if (isInSightRange && !isInAttackRange)
+        {
+            DetectPlayer();
+        }
+        else if (isInSightRange && isInAttackRange)
+        {
+            AttackPlayer();
+        }
+    }
+
+    private void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+        transform.LookAt(playerTransform);
+
+        if (!isAttacking)
+        {
+            isAttacking = true;
+            Invoke("ResetAttack", attackDelay);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        isAttacking = false;
+    }
+
+    private void DetectPlayer()
+    {
+        agent.SetDestination(playerTransform.position);
+        transform.LookAt(playerTransform);
+    }
+
+    private void Patrolling()
+    {
+        if (!walkPointSet)
+        {
+            float randomXPos = UnityEngine.Random.Range(-walkPointRange, walkPointRange);
+            float randomZPos = UnityEngine.Random.Range(-walkPointRange, walkPointRange);
+            walkPoint = new Vector3(transform.position.x + randomXPos, transform.position.y, transform.position.z + randomZPos);
+
+            if (Physics.Raycast(walkPoint, -transform.up, 2f, groundLayer))
+            {
+                walkPointSet = true;
+            }
+        }
+
+        if (walkPointSet)
+        {
+            agent.SetDestination(walkPoint);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        if (distanceToWalkPoint.magnitude < 1f)
+        {
+            walkPointSet = false;
+        }
     }
 
     public void GetDamage(int damage)
@@ -37,5 +122,13 @@ public class EnemyManager : MonoBehaviour
     private void EnemyDied()
     {
         Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
     }
 }
